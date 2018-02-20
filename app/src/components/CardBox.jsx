d161841efbc934f5e76abe6848a2f77d07037f23
cwxyz007 <jie844067636@gmail.com>
2018-02-20 22:19:55 +0800

    show picture size

--- app/src/components/CardBox.jsx
+++ app/src/components/CardBox.jsx
@@ -1,15 +1,34 @@
 import React, { Component } from 'react'
+import fs from 'fs'
+import $ from 'jquery'
 
 export default class CardBox extends Component {
   constructor(props) {
     super(props)
+    this.state= {
+      infos: props.infos || []
+    }
+  }
+
+  componentDidMount(){
+    $(this.image).on('load', () => {
+      this.setState((prevState) => {
+        let state = fs.statSync($(this.image).attr('src'))
+        if(state){
+          let size = state.size / 1000;
+          let sizeStr = size >= 1000 ? (size / 1000).toFixed(1) + "MB" : size.toFixed(1) + "KB"
+          prevState.infos.push(sizeStr)
+        }
+        return {infos: prevState.infos}
+      })
+    })
   }
 
   render() {
     return (
       <div className='card-box'>
         <div className='picture'>
-          <img src={this.props.src} />
+          <img src={this.props.src} ref={img => this.image = img}/>
         </div>
         <div className='info-box'>
           <div className='tags'>
@@ -19,8 +38,9 @@ export default class CardBox extends Component {
             }
           </div>
           <p className='infos'>
-            <span className='info'>test</span>
-            <span className='info'>test</span>
+            {
+              this.state.infos.map((info, index) => <span className='info' key={index}>{info}</span>)
+            }
           </p>
         </div>
       </div>
